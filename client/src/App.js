5a478d7c896881fb8a9318c5785ae0091ad778a3
irina mihai <irinamihai150@gmail.com>
2023-02-14 16:36:08 +0000

    list of students and scores, api endpointfakedata

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 
+
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Mentor from "./pages/Mentor";
 
 const App = () => (
+
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/mentor" element={<Mentor />} />
 	</Routes>
 );
 
