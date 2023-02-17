e691fef0533b48db1c49c35de228f56d9a7f4c1a
KawaManmi <kawamam2007@gmail.com>
2023-02-17 23:49:30 +0000

    login page linked with Student+Mentor page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./Home.css";
 import Login from "./Login";
 import Header from "./Header";
@@ -7,28 +7,28 @@ import Student from "./Student";
 import Mentor from "./Mentor";
 
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
+	// const [message, setMessage] = useState("Loading...");
 	const [loginResponse, setLoginResponse] = useState("");
 
 	const handleLogin = (response) => {
 		setLoginResponse(response);
 	};
 
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
+	// useEffect(() => {
+	// 	fetch("/api")
+	// 		.then((res) => {
+	// 			if (!res.ok) {
+	// 				throw new Error(res.statusText);
+	// 			}
+	// 			return res.json();
+	// 		})
+	// 		.then((body) => {
+	// 			setMessage(body.message);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.error(err);
+	// 		});
+	// }, []);
 
 	return (
 		<main role="main">
