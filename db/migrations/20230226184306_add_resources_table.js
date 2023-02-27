29895df160efd4c26c3d1105f5b4cd756f86ec5d
Hayat <hayatu4islam@yahoo.com>
2023-02-27 10:47:19 +0000

    created migration and seeds for resources table

---
+++ db/migrations/20230226184306_add_resources_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("resources",
+  function (table){
+    table.increments("id");
+    table.string("title");
+    table.string("url");
+    table.integer("reading_time");
+    table.integer("skill_id").references("id").inTable("skills").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("resources");
+};
