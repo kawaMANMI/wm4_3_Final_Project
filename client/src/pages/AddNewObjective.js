9507744347066e2f3ee6d77c23e9f659a2a260d4
irina mihai <irinamihai150@gmail.com>
2023-02-27 12:46:56 +0000

    filter by region, updated table with scores per each skill

--- client/src/pages/AddNewObjective.js
+++ client/src/pages/AddNewObjective.js
@@ -2,27 +2,29 @@ import React, { useState } from "react";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-function AddNewObjective({ handleSubmitObj }) {
-	const [skill, setSkill] = useState("");
-	const [obj, setObj] = useState("");
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import "./LearningObj.css";
 
-	function handleChangeSkill(e) {
-		setSkill(e.target.value);
-	}
+function AddNewObjective({ handleSubmitObj }) {
+	const [obj, setObj] = useState("");
+	const [selectedOption, setSelectedOption] = useState("option1");
+	const handleOptionChange = (event) => setSelectedOption(event.target.value);
+
 	function handleChangeObj(e) {
 		setObj(e.target.value);
 	}
 
-	const handleSubmitPost = async (e, skill_id, objective) => {
+	const handleSubmitPost = async (e, objective) => {
 		e.preventDefault();
 		try {
 			const res = await axios.post("/api/learning_objectives", {
-				skill_id: skill,
+				skill_id: selectedOption.slice(-1),
 				objective: obj,
 			});
 			if (res.status === 200) {
 				const newObjective = {
-					skill_id: skill,
+					skill_id: selectedOption.slice(-1),
 					objective: objective,
 				};
 				handleSubmitObj(newObjective);
@@ -35,29 +37,57 @@ function AddNewObjective({ handleSubmitObj }) {
 		}
 	};
 	return (
-		<div style={{ textAlign: "center" }}>
-			<Form style={{ width: "18em" }} onSubmit={handleSubmitPost}>
-				<h5 style={{ color: "red" }}>Add New Learning Objective</h5>
+		<div className="d-flex align-items-center">
+			<Form onSubmit={handleSubmitPost}>
+				<DropdownButton
+					id="dropdown-basic-button"
+					title={`Skill ${selectedOption}`}
+				>
+					<Dropdown.Item
+						onClick={() => handleOptionChange({ target: { value: "option1" } })}
+					>
+						Option 1
+					</Dropdown.Item>
+					<Dropdown.Item
+						onClick={() => handleOptionChange({ target: { value: "option2" } })}
+					>
+						Option 2
+					</Dropdown.Item>
+					<Dropdown.Item
+						onClick={() => handleOptionChange({ target: { value: "option3" } })}
+					>
+						Option 3
+					</Dropdown.Item>
+					<Dropdown.Item
+						onClick={() => handleOptionChange({ target: { value: "option4" } })}
+					>
+						Option 4
+					</Dropdown.Item>
+					<Dropdown.Item
+						onClick={() => handleOptionChange({ target: { value: "option5" } })}
+					>
+						Option 5
+					</Dropdown.Item>
+					<Dropdown.Item
+						onClick={() => handleOptionChange({ target: { value: "option6" } })}
+					>
+						Option 6
+					</Dropdown.Item>
+				</DropdownButton>
+
 				<Form.Group className="mb-3">
-					<Form.Label>Skill Id</Form.Label>
 					<Form.Control
+						className="form-control"
+						style={{ marginTop: "1em" }}
 						required
-						type="text"
-						value={skill}
-						onChange={handleChangeSkill}
-					/>
-				</Form.Group>
-				<Form.Group className="mb-3">
-					<Form.Label> Learning Objective</Form.Label>
-					<Form.Control
-						required
+						placeholder=" New Learning Objective"
 						type="text"
 						value={obj}
 						onChange={handleChangeObj}
 					/>
 				</Form.Group>
-				<Button variant="primary" type="submit">
-					Submit
+				<Button variant="primary" type="submit" className="btn-block">
+					Submit Objective
 				</Button>
 			</Form>
 		</div>
