c9410adf9a42d06c571737e1de9c5b7617ae9013
Hayat <hayatu4islam@yahoo.com>
2023-03-03 15:42:31 +0000

    Added resources card

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -1,18 +1,50 @@
-import React from "react";
-// import React, { useState, useEffect } from "react";
+// import React from "react";
+import React, { useState, useEffect } from "react";
 // import { Card, Row, Col, Accordion } from "react-bootstrap";
-// import axios from "axios";
-import Alert from "react-bootstrap/Alert";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function Resources() {
-    // const [resources, setResources] = useState({});
+    const profileId = location.state ? location.state.studentId : "";
+    const [resources, setResources] = useState([]);
+    const id = sessionStorage.getItem("userId");
+    useEffect(() => {
+			axios
+				.get(`/api/resources/${profileId}`)
+				.then((response) => {
+					if (response.status === 200) {
+						return response.data;
+					} else {
+						throw new Error("Something is wrong");
+					}
+				})
+				.then((data) => {
+					setResources(data);
+				})
+				.catch((e) => console.log({ error: e.message }));
+		}, [profileId]);
+        console.log("Resources", resources);
     return (
-        <Alert dismissible variant='danger'>
-            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-            <p>
-                Change this and not that and try again.
-            </p>
-        </Alert>
-    );
+			<Card>
+				<Card.Header>Resources</Card.Header>
+				<Card.Body>
+					<Card.Title>Recommended Resources</Card.Title>
+					<Card.Text>
+						Based on your current score level, we recommend the following
+						resources:
+					</Card.Text>
+					<ListGroup variant="flush">
+						{resources.map((variant) => (
+							<a href={variant.url} key={variant.url}>
+								<ListGroup.Item key={variant.title}>
+									{variant.title}
+								</ListGroup.Item>
+							</a>
+						))}
+					</ListGroup>
+				</Card.Body>
+			</Card>
+		);
 }
 export default Resources;
