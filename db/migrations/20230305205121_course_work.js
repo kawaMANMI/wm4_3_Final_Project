6726303b62f4b30eabc23a7748af77b4f9b3e509
Hayat <hayatu4islam@yahoo.com>
2023-03-05 21:59:54 +0000

    Fixed seeds

--- db/migrations/20230305205121_course_work.js
+++ db/migrations/20230305205121_course_work.js
@@ -2,19 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.createTable("coursework",
-  function (table) {
-    table.increments("id");
-    table.string("coursework");
-    table.integer("course_skill").references("id").inTable("skills").notNullable();
-  });
+exports.up = function (knex) {
+	return knex.schema.createTable("coursework", function (table) {
+		table.increments("id");
+		table.string("coursework");
+		table
+			.integer("course_skill")
+			.references("id")
+			.inTable("skills")
+			.notNullable();
+	});
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("coursework");
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExists("coursework");
 };
