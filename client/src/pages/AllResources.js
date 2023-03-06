70d62dccdd97b2bbe84afbf5930806a3f8451011
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2023-03-06 20:22:05 +0000

    change-skill-banner-display

--- client/src/pages/AllResources.js
+++ client/src/pages/AllResources.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Row, Card, Accordion, Col, Collapse } from "react-bootstrap";
+import { Row, Card, Col, Collapse } from "react-bootstrap";
 import skills from "./Skill.json";
 import "./AllResources.css";
 
@@ -28,58 +28,48 @@ function AllResources() {
 			>
 				More Resources and Assessment
 			</Card.Header>
-			<Accordion defaultActiveKey="0">
-				<Accordion.Item eventKey="1">
-					<Accordion.Header>
-						<strong
-							style={{
-								color: "#DC143C",
-								textShadow: "1px 1px 1px grey",
-							}}
+			<Card.Body style={{ padding: "20px" }}>
+				<Row className="g-4 mt-2 mb-2">
+					{skills.map((skill) => (
+						<Col
+							sm={5}
+							md={6}
+							lg={4}
+							key={skill.id}
+							className="d-flex justify-content-center"
 						>
-							Select the Skill
-						</strong>
-					</Accordion.Header>
-					<Accordion.Body>
-						<Row className="g-4">
-							{skills.map((skill) => (
-								<Col
-									sm={5}
-									md={6}
-									lg={4}
-									key={skill.id}
-									className="d-flex justify-content-center"
-								>
-									<Card border="light" className="card-with-fixed-height">
-										<Card.Img
-											variant="top"
-											src={skill.url}
-											crossOrigin="anonymous"
-											onClick={() => handleCardClick(skill.id)}
-											aria-controls={skill.id}
-											aria-expanded={openCollapseId === skill.id}
-										/>
-										<Collapse in={openCollapseId === skill.id}>
-											<div id={skill.id}>
-												<div className="d-flex justify-content-around mb-2 mt-2">
-													<Link
-														to={`/more-resources/${skill.title}/${skill.id}`}
-													>
-														Resources
-													</Link>
-													<Link to={`/assessment/${skill.title}/${skill.id}`}>
-														Assessment
-													</Link>
-												</div>
-											</div>
-										</Collapse>
-									</Card>
-								</Col>
-							))}
-						</Row>
-					</Accordion.Body>
-				</Accordion.Item>
-			</Accordion>
+							<Card border="light" className="card-with-fixed-height">
+								<Card.Img
+									variant="top"
+									src={skill.url}
+									crossOrigin="anonymous"
+									onClick={() => handleCardClick(skill.id)}
+									aria-controls={skill.id}
+									aria-expanded={openCollapseId === skill.id}
+								/>
+								<Collapse in={openCollapseId === skill.id}>
+									<div id={skill.id}>
+										<div className="d-flex justify-content-around mb-2 mt-2">
+											<Link
+												to={`/more-resources/${skill.title}/${skill.id}`}
+												style={{ color: "#DC143C" }}
+											>
+												Resources
+											</Link>
+											<Link
+												to={`/assessment/${skill.title}/${skill.id}`}
+												style={{ color: "#DC143C" }}
+											>
+												Assessment
+											</Link>
+										</div>
+									</div>
+								</Collapse>
+							</Card>
+						</Col>
+					))}
+				</Row>
+			</Card.Body>
 		</Card>
 	);
 }
